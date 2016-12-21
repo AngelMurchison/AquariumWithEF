7fb9f56d4eb576073b0a9958394732aa1adbb49a
Angel Murchison <angelmurchison09@gmail.com>
2016-12-21 17:22:34 -0500

    Database restructured. CRUD functionality defined as methods. Added some comments, and new query syntaxxiums.

--- EFAquarium/Program.cs
+++ EFAquarium/Program.cs
@@ -60,7 +60,7 @@ namespace EFAquarium
             var db = new AquariumContext();
             var aqCreature = new AquariumCreatures() { CreatureID = creatureID, AquariumID = aquariumID, OceanID = oceanID };
             db.AquariumCreatures.Add(aqCreature);
-            //db.SaveChanges();
+            db.SaveChanges();
             db = null;
         }
 
@@ -69,7 +69,7 @@ namespace EFAquarium
             var db = new AquariumContext();
             var toDelete = db.AquariumCreatures.First(f => f.Id == AqCreatureID);
             db.AquariumCreatures.Remove(toDelete);
-            //db.SaveChanges();
+            db.SaveChanges();
             db = null;
         }
 
@@ -79,6 +79,7 @@ namespace EFAquarium
             var v = from AquariumCreatures in db.AquariumCreatures
                     join Aquariums in db.Aquariums
                     on AquariumCreatures.Aquarium.name equals Aquariums.name
+                    where Aquariums.name == aquariumname
                     select new { AquariumCreatures.Creature, Aquariums.name };
             foreach (var item in v)
             {
@@ -109,18 +110,20 @@ namespace EFAquarium
             db = null;
         }
 
-        public static void distinctCitiesWithAquariums(string cityname)
+        public static void distinctCitiesWithAquariums()
         {
             var db = new AquariumContext();
             var v = from AquariumCreatures in db.AquariumCreatures
                        join aq in db.Aquariums on AquariumCreatures.AquariumID equals aq.Id
                        join c in db.Creatures on AquariumCreatures.CreatureID equals c.Id
                        join o in db.Oceans on AquariumCreatures.OceanID equals o.Id
-                       where aq.city == $"{cityname}"
-                       select new { aquarium = aq.city};
+                       select new { aquarium = aq.city };
             var distinct = v.Distinct();
-            Console.WriteLine(db.Aquariums.Single().city.ToString());
-
+            foreach (var item in distinct)
+            {
+                item.ToString();
+                Console.WriteLine(item);
+            }
             Console.WriteLine("Press any key to continue");
             Console.ReadLine();
             db = null;
@@ -133,12 +136,32 @@ namespace EFAquarium
             var test = from AquariumCreatures in db.AquariumCreatures
                        join aq in db.Aquariums on AquariumCreatures.AquariumID equals aq.Id
                        join c in db.Creatures on AquariumCreatures.CreatureID equals c.Id
-                       join o in db.Oceans on AquariumCreatures.OceanID equals o.Id
+                       join o in db.Oceans.Distinct() on AquariumCreatures.OceanID equals o.Id
                        select new { ocean = o.name, creature = c };
+            var arcticcounter = 0;
+            var pacificcounter = 0;
+            var atlanticcounter = 0;
+            var southcounter = 0;
+            var indiancounter = 0;
             foreach (var item in test)
             {
                 Console.WriteLine(item);
-            }
+                var arcticbool = item.ocean.Equals("Arctic");
+                if (arcticbool) { arcticcounter++; }
+                var pacificbool = item.ocean.Equals("Pacific");
+                if (pacificbool) { pacificcounter++; }
+                var atlanticbool = item.ocean.Equals("Atlantic");
+                if (atlanticbool) { atlanticcounter++; }
+                var southbool = item.ocean.Equals("South");
+                if (southbool) { southcounter++; }
+                var indianbool = item.ocean.Equals("Indian");
+                if (indianbool) { indiancounter++; }
+            };
+            Console.WriteLine($"Arctic: {arcticcounter}");
+            Console.WriteLine($"Pacific: {pacificcounter}");
+            Console.WriteLine($"Atlantic: {atlanticcounter}");
+            Console.WriteLine($"South: {southcounter}");
+            Console.WriteLine($"Indian: {indiancounter}");
 
             Console.WriteLine("Press any key to continue");
             Console.ReadLine();
@@ -147,47 +170,29 @@ namespace EFAquarium
 
         static void Main(string[] args)
         {
-            var db = new AquariumContext();
+            /* Create */
+            //createAqCreature(3, 1, 3);
 
-            //var creature = db.Creatures.First();
-            //var creature2 = db.Creatures.First(f => f.Id.Equals(2)) as Creature;
-            //var creature3 = db.Creatures.First(f => f.Id.Equals(3)) as Creature;
-            //var creature4 = db.Creatures.First(f => f.Id.Equals(4)) as Creature;
-            //var aquarium = db.Aquariums.First();
-            //var ocean = db.Oceans.First();
+            /* Update */
+            //updateAqCreature(9, 2, 3, 1);
 
-            //var removethis = db.AquariumCreatures.First(c => c.CreatureID == null);
-            //db.AquariumCreatures.Remove(removethis);
-            //db.SaveChanges();
+            /* Delete */
+            //deleteAqCreature(8);
 
-            // first creature, aq, ocean. create new aquatic creature, give it those items as properties
-
-            // Create
-            createAqCreature(1, 1, 1);
-
-            // Read
+            /* Read */
             readAqCreature();
 
-            // Update
-            updateAqCreature(5, 1, 1, 2);
-            updateAqCreature(6, 1, 1, 3);
-            updateAqCreature(7, 1, 1, 4);
-            //db.SaveChanges();
-
-            // Delete
-            deleteAqCreature(1);
-
             // An SQL Query that given an Aquarium Name, What AquaticLife is there
-            Console.Write("new query \n");
+            Console.Write("All Aquatic Life at the Tampa Aquarium \n");
             creaturesFromAquariumName("The Tampa Aquarium");
 
             // An SQL Query that, given an Ocean, What Aquariums have fish from that ocean
-            Console.Write("new query \n");
+            Console.Write("All fish in the Arctic \n");
             creaturesAndTheirAquariumsFromOcean("Arctic");
 
             // An SQL Query that Returns Only the Distinct (new topic) Cities that have aquariums
-            Console.Write("new query \n");
-            distinctCitiesWithAquariums("Tampa");
+           // Console.Write("new query \n");
+             distinctCitiesWithAquariums();
 
             // An SQL Query that Gives the Count(new topic) of How many species of AquaticLife live in each Ocean
             aquaticLifeInEachOcean();
