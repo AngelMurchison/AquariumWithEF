be0b69e34b9f61234074b177ad50c31eed25a1ba
Angel Murchison <angelmurchison09@gmail.com>
2016-12-21 10:40:43 -0500

    Database restructured. CRUD functionality defined as methods. Added some comments, and new query syntaxxiums

--- EFAquarium/Program.cs
+++ EFAquarium/Program.cs
@@ -22,11 +22,25 @@ namespace EFAquarium
         public static void readAqCreature()
         {
             var db = new AquariumContext();
-            var aqcreatures = db.AquariumCreatures.ToList();
-            printCollection(aqcreatures);
+            // var aqcreatures = db.AquariumCreatures.ToList();
+            //foreach (var item in db.AquariumCreatures)
+            //{
+            // var joineditem = db.AquariumCreatures.First(f => f.Id == item.Id);
+            // Console.WriteLine(item);
+            // }
+            var test = db.AquariumCreatures.Join
+                       (db.Oceans, fk => fk.OceanID, pk => pk.Id, (aqc, o) =>
+                       new { AquariumCreatures = aqc, Ocean = o })
+                       .Join(db.Creatures, fk => fk.AquariumCreatures.CreatureID, pk => pk.Id, (a, c) =>
+                       new { AquariumCreatures = a.AquariumCreatures, Ocean = a.Ocean, Creature = c })
+                       .Join(db.Aquariums, fk => fk.AquariumCreatures.AquariumID, pk => pk.Id, (a, aq) =>
+                       new {AquariumCreatures = a.AquariumCreatures, /*Ocean = a.Ocean, Creature = a.Creature,*/ Aquarium = aq });
+                                            
+
             Console.ReadLine();
             db = null;
         }
+
         public static void updateAqCreature(int AqCreatureID = 1, int aquariumID = 1, int creatureID = 1, int oceanID = 1) // how to make default values unchanged, idt actually updates
         {
             var db = new AquariumContext();
@@ -34,23 +48,25 @@ namespace EFAquarium
             toUpdate.AquariumID = aquariumID;
             toUpdate.CreatureID = creatureID;
             toUpdate.OceanID = oceanID;
-            db.SaveChanges();
+            //db.SaveChanges();
             db = null;
         }
+
         public static void createAqCreature(int creatureID = 1, int aquariumID = 1, int oceanID = 1)
         {
             var db = new AquariumContext();
             var aqCreature = new AquariumCreatures() { CreatureID = creatureID, AquariumID = aquariumID, OceanID = oceanID };
             db.AquariumCreatures.Add(aqCreature);
-            db.SaveChanges();
+            //db.SaveChanges();
             db = null;
         }
+
         public static void deleteAqCreature(int AqCreatureID)
         {
             var db = new AquariumContext();
             var toDelete = db.AquariumCreatures.First(f => f.Id == AqCreatureID);
             db.AquariumCreatures.Remove(toDelete);
-            db.SaveChanges();
+            //db.SaveChanges();
             db = null;
         }
 
@@ -76,38 +92,34 @@ namespace EFAquarium
             //var aquarium = db.Aquariums.First();
             //var ocean = db.Oceans.First();
 
-            //var aqCreature = new AquariumCreatures() { Creature = creature, Aquarium = aquarium, Ocean = ocean };
-            //var aqCreature2 = new AquariumCreatures() { Creature = null, Aquarium = aquarium, Ocean = ocean };
-            //var aqCreature3 = new AquariumCreatures() { Creature = null, Aquarium = aquarium, Ocean = ocean };
-            //var aqCreature4 = new AquariumCreatures() { Creature = null, Aquarium = aquarium, Ocean = ocean };
-
             //var removethis = db.AquariumCreatures.First(c => c.CreatureID == null);
             //db.AquariumCreatures.Remove(removethis);
             //db.SaveChanges();
 
             // first creature, aq, ocean. create new aquatic creature, give it those items as properties
 
-            //// Create
-            //createAqCreature(1, 1, 1);
+            // Create
+            createAqCreature(1, 1, 1);
 
-            //// Read
-            //readAqCreature();
+            // Read
+            readAqCreature();
 
-            //// Update
-            //updateAqCreature(1, 1, 1, 1);
+            // Update
+            updateAqCreature(1, 1, 1, 1);
 
-            //// Delete
-            //deleteAqCreature(1);
+            // Delete
+            deleteAqCreature(1);
 
             // An SQL Query that given an Aquarium Name, What AquaticLife is there
-
-            var sql1 = creaturesFromAquariumName("The Tampa Aquarium");
+            var sql1 = creaturesFromAquariumName("The Tampa Aquarium").ToListAsync();
+            Console.WriteLine(sql1);
+            Console.ReadLine();
 
 
             //var creaturesfromaquarium = creaturesFromAqName.ToList();
             //foreach (var creature in creaturesfromaquarium)
             //{
-            //      Console.WriteLine(creature);
+            //    Console.WriteLine(creature);
             //}
 
             //Console.ReadLine();
@@ -118,7 +130,11 @@ namespace EFAquarium
             //        select new { AquariumCreatures.Creature, Aquariums.name };
 
             // An SQL Query that, given an Ocean, What Aquariums have fish from that ocean
-            var fishFromOcean = 
+            //var fishFromOcean = from AquariumCreatures in db.AquariumCreatures
+            //                    join Ocean in db.Oceans
+            //                    on AquariumCreatures.Ocean.name equals Ocean.name
+
+            //                    into 
 
             // An SQL Query that Returns Only the Distinct (new topic) Cities that have aquariums
             // An SQL Query that Gives the Count(new topic) of How many species of AquaticLife live in each Ocean
